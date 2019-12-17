362da01b9c8cbcfa8bda513d0a3303a3d3793bb9
Shaun Simpson <shaungsimpson@gmail.com>
2019-12-18 09:01:14 +1100

    Added Enqueue class for handling script and style enqueueing

---
+++ inc/Core/Enqueue.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package    CashbackFAQs
+ */
+namespace Inc\Core;
+
+class Enqueue {
+
+  define( CSS_PATH, '/cashback-claims/assets/css/' );
+  define( JS_PATH, '/cashback-claims/assets/js/' );
+
+  public function register() {
+    add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue' ) );
+    // add_action( 'wp_enqueue_scripts', array( $this, 'public_enqueue' ) );
+  }
+
+  // enqueue admin side scripts and styles.
+  function admin_enqueue() {
+    wp_enqueue_style( 'cbc_admin_styles', plugins_url( CSS_PATH . 'admin-styles.css'); );
+  }
+
+  // enqueue front end scripts and styles.
+  function public_enqueue() {
+    // wp_enqueue_style( 'cbc_admin_styles', plugins_url( '/cashback-claims/assets/css/admin-styles.css'); );
+  }
+
+}
