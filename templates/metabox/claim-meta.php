3bd86ffe64be298e5e58a7e33fea510aefd835a7
Shaun Simpson <shaungsimpson@gmail.com>
2019-12-18 20:43:36 +1100

    Refactored claim metabox.

--- templates/metabox/claim-meta.php
+++ templates/metabox/claim-meta.php
@@ -2,114 +2,110 @@
 /**
  * Template for metabox on claim CPT allowing to save/see metadata
  */
-  // $claim = array(
-  //   'first-name',
-  //   'last-name',
-  //   'company-name',
-  //   'mobile',
-  //   'email',
-  //   'address-1',
-  //   'address-2',
-  //   'suburb',
-  //   'claim-state',
-  //   'postcode',
-  //   'claim-date',
-  //   'terms'
-  // );
 
-  // $purchase = array(
-  //   "purchase-date",
-  //   "purchase-state",
-  //   "purchase-location",
-  //   "invoice-number",
-  //   "invoice-img"
-  // );
+// array of form fields for helper functions ( input_field() and state_select() )
+// format is '<meta_key>' => array( 'key' => '<meta_key>', 'label' => '<label string>', 'type' => '<input type>', 'required' => <bool> ),
+$meta_fields = array(
+  'first-name' => array( 'key' => 'first-name', 'label' => 'First Name', 'type' => 'text', 'required' => true ),
+  'last-name' => array( 'key' => 'last-name', 'label' => 'Last Name', 'type' => 'text', 'required' => true ),
+  'company-name' => array( 'key' => 'company-name', 'label' => 'Company', 'type' => 'text', 'required' => false ),
+  'mobile' => array( 'key' => 'mobile', 'label' => 'Mobile', 'type' => 'tel', 'required' => true ),
+  'email' => array( 'key' => 'email', 'label' => 'Email', 'type' => 'email', 'required' => true ),
+  'address-1' => array( 'key' => 'address-1', 'label' => 'Address 1', 'type' => 'text', 'required' => true ),
+  'address-2' => array( 'key' => 'address-2', 'label' => 'Address 2', 'type' => 'text', 'required' => false ),
+  'suburb' => array( 'key' => 'suburb', 'label' => 'Suburb', 'type' => 'text', 'required' => true ),
+  'claim-state' => array( 'key' => 'claim-state', 'label' => 'Claim State', 'type' => 'select', 'required' => true ),
+  'postcode' => array( 'key' => 'postcode', 'label' => 'Postcode', 'type' => 'text', 'required' => true ),
+  'claim-date' => array( 'key' => 'claim-date', 'label' => 'Date of Claim', 'type' => 'date', 'required' => true ),
+  'terms' => array( 'key' => 'terms', 'label' => 'Agreed to terms', 'type' => 'checkbox', 'required' => true ),
+  "purchase-date" => array( 'key' => "purchase-date", 'label' => 'Date of Purchase', 'type' => 'date', 'required' => true ),
+  "purchase-state" => array( 'key' => "purchase-state", 'label' => 'State of Purchase', 'type' => 'select', 'required' => true ),
+  "purchase-location" => array( 'key' => "purchase-location", 'label' => 'Purchase Location', 'type' => 'text', 'required' => false ),
+  "invoice-number" => array( 'key' => "invoice-number", 'label' => 'Invoice Number', 'type' => 'text', 'required' => true ),
+  "invoice-img" => array( 'key' => "invoice-img", 'label' => 'Uploaded Invoice', 'type' => 'file', 'required' => true ),
+);
 
-$states = array('NSW', 'QLD', 'VIC', 'WA', 'SA', 'TAS');
+function input_field( $meta_field ) {
+  global $post_id;
+  $value = get_post_meta($post_id, $meta_field['key'], true) ?? '';
+  ?>
+  <label><?= $meta_field['label'] ?></label>
+  <input type="<?= $meta_field['type'] ?>" name="<?= $meta_field['key'] ?>" value="<?= $value ?>"<?= $meta_field['required'] ? ' required' : '' ?>>
+<?php }
+
+function state_select( $meta_field ) {
+  global $post_id;
+  $states = array('NSW', 'QLD', 'VIC', 'WA', 'SA', 'TAS');
+  $value = get_post_meta($post_id, $meta_field['key'], true) ?? '';
+  ?>
+    <label><?= $meta_field['label'] ?></label>
+    <select name="<?= $meta_field['key'] ?>" <?= $meta_field['required'] ? 'required ' : '' ?>>
+      <?php foreach ($states as $state) { ?>
+        <option value="<?= $state ?>" <?= selected( $meta_field['key'], $state, true, 'selected' ) ?>><?= $state ?></option>
+      <?php } ?>
+    </select>
+  <?php }
 ?>
 <h4>Personal Details</h4>
 <div class="row">
   <div class="half">
-    <label>First Name</label>
-    <input type="text" name="first-name" required="required">
+    <?php input_field( $meta_fields['first-name']); ?>
   </div>
   <div class="half">
-    <label>Last Name</label>
-    <input type="text" name="last-name" required="required">
+    <?php input_field( $meta_fields['last-name']); ?>
   </div>
 </div>
 <div class="row">
-  <label>Company Name</label>
-  <input type="text" name="company-name">
+  <?php input_field( $meta_fields['company-name']); ?>
 </div>
 <div class="row">
   <div class="half">
-    <label>Mobile</label>
-    <input type="tel" name="mobile" required="required">
+    <?php input_field( $meta_fields['mobile']); ?>
   </div>
   <div class="half">
-    <label>Email</label>
-    <input type="email" name="email" required="required">
+    <?php input_field( $meta_fields['email']); ?>
   </div>
 </div>
 <div class="row">
-  <label>Address 1</label>
-  <input type="text" name="address-1" required="required">
+  <?php input_field( $meta_fields['address-1']); ?>
 </div>
 <div class="row">
-  <label>Address 2</label>
-  <input type="text" name="address-2">
+  <?php input_field( $meta_fields['address-2']); ?>
 </div>
 <div class="row">
   <div class="half">
-    <label>Suburb</label>
-    <input type="text" name="suburb" required="required">
+    <?php input_field( $meta_fields['suburb']); ?>
   </div>
   <div class="half">
-    <label>Claim State</label>
-    <select name="claim-state" required="required">
-      <?php foreach ($states as $state) { ?>
-        <option value="<?= $state ?>"><?= $state ?></option>
-      <?php } ?>
-    </select>
+    <?= state_select( $meta_fields['claim-state'] ) ?>
   </div>
 </div>
 <div class="row">
   <div class="half">
-    <label>Postcode</label>
-    <input type="number" name="postcode" required="required">
+    <?php input_field( $meta_fields['postcode']); ?>
   </div>
   <div class="half">
-    <label>Claim Date</label>
-    <input type="date" name="claim-date" required="required">
+    <?php input_field( $meta_fields['claim-date']); ?>
   </div>
 </div>
 <div class="row">
-  <label><input type="checkbox" name="terms" required="required">Terms and conditions agreed to</label>
-</div>
-<hr>
-<h4>Purchase Details</h4>
-<div class="row">
-  <div class="half">
-    <label>Purchase Date</label>
-    <input type="text" name="purchase-date" required="required">
+  <label><input type="checkbox" name="terms" required="required">Terms and conditions agreed to</label value="">
   </div>
-  <div class="half">
-    <label>Purchase State</label>
-    <select name="purchase-state" required="required">
-      <?php foreach ($states as $state) { ?>
-        <option value="<?= $state ?>"><?= $state ?></option>
-      <?php } ?>
-    </select>
+  <hr>
+  <h4>Purchase Details</h4>
+  <div class="row">
+    <div class="half">
+      <?php input_field( $meta_fields['purchase-date']); ?>
+    </div>
+    <div class="half">
+      <?= state_select( $meta_fields['purchase-state'] ) ?>
+    </div>
   </div>
-</div>
-<div class="row">
-  <div class="half">
-    <label>Purchase Location</label>
-    <input type="text" name="purchase-location">
+  <div class="row">
+    <div class="half">
+      <?php input_field( $meta_fields['purchase-location']); ?>
+    </div>
+    <div class="half">
+      <?php input_field( $meta_fields['invoice-number']); ?>
+    </div>
   </div>
-  <div class="half">
-    <label>Invoice Number</label>
-    <input type="text" name="invoice-number" required="required">
-  </div>
-</div>
